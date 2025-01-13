7adb6ccf9b2c61d22cc31a033595cfe5c7a8288b
CBJ <200110705@stu.hit.edu.cn>
2025-01-13 13:33:56 +0800

    feat: add backup.sh

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Error: Missing argument"
+  echo "Usage: $0 <src_dir> <working_dir>"
+  echo "src_dir can be a directory or a list of file paths"
+  exit 1
+fi
+
+. ./utils.sh
+
+testInit $2
+CONFIG=-p"working-directory $WORKING_DIR"
+SRC_DIR=$1
+
+cd ~/destor
+remake
+
+set -ex
+
+resetAll
+
+# basic test
+mkdir -p ${DST_DIR}${RESTORE_ID}
+./destor ${SRC_DIR} "${CONFIG}" > ${LOG_DIR}/0.log
